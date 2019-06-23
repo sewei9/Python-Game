9a35c09f16d7a1a17ab9a421d2ac556761abfe56
sewei9 <sebastianweiss2@gmx.de>
2019-06-23 15:20:41 +0200

    Übung EIngabe inch added

---
+++ eingabe/Übung_eingabe_inch.py
@@ -0,0 +1,13 @@
+#Inch in cm
+inch = 2.54
+
+#Eingabe
+print("Wandeln sie inch in cm um:")
+z = float(input())
+
+# Eingabe in cm umwandeln
+cm = z * inch
+
+#Ausgabe
+print("Ihre Zahl in inch:", z)
+print("Ihre Zahl in cm:", cm)
