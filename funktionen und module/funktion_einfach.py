c919c4761262347908b1dd509ebed9a1ce695dc2
sewei9 <sebastianweiss2@gmx.de>
2019-06-23 17:04:54 +0200

    Create funktion_einfach.py

---
+++ funktionen und module/funktion_einfach.py
@@ -0,0 +1,16 @@
+#Definition der Funktion
+def stern():
+    print("-----------------")
+    #print("*** Trennung ****")
+    #print("-----------------")
+
+#Hauptprogramm
+x = 12
+y = 5
+#stern()
+print("x =", x, ", y=", y)
+stern()
+print("x + y = ", x + y)
+#stern()
+print("x - y = ", x - y)
+#stern()
