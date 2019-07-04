6c4fd82fab0a4b5eb0c5bf66e074fcaf5860fa18
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-07-04 10:17:22 +0200

    String ops, seek, unchangable

--- Datatypes/Operationen/text_operationen.py
+++ Datatypes/Operationen/text_operationen.py
@@ -1,3 +1,27 @@
 # Beispielsequenz, hier Zeichenkette
 tname = "Robinson Crusoe"
 print("Text:", tname)
+
+# Anzahl Elemente
+lg = len(tname)
+print("Anzahl Elemente:", lg)
+
+# Teilbereiche, Elemente
+ts = tname[5:8]  # Slice von ...bis
+print("[5:8]:", ts)
+
+ts = tname[:8]  # Slice bis
+print("[:8]:", ts)
+
+ts = tname[9:]  # Slice von
+print("[9:]:", ts)
+
+ts = tname[9]
+print("[9]:", ts)
+
+ts = tname[9:-3]
+print("[9:-3]:", ts)
+
+# Elemente einzeln
+for zeichen in tname[5:8]: # Kann hier genutzt werden um eine ganze Zeichenkette oder einen Teil der Zeichenkette Zeichen für Zeichen durchzugehen
+    print(zeichen)
