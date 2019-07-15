30ee2864c41653457dda0bc6e26af50bd0754644
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-07-15 10:02:25 +0200

    Referenz, Identität, Kopie

---
+++ Datatypes/none.py
@@ -0,0 +1,27 @@
+# Funktion
+def quotient(a, b):
+    try:
+        c = a/b
+        return c
+    except:
+        print("Funktion meldet Fehler")
+
+# liefert Ergebnis
+erg = quotient(7,4)
+if erg:
+    print("Ergebnis:", erg)
+print()
+
+# liefert Fehler
+erg = quotient(7,0)
+if not erg:
+    print("Programm meldet Fehler")
+print("Ergebnis:", erg)
+print("Typ des Ergebnisses:", type(erg))
+print()
+
+# Konstante None
+z = None
+print("z:", z)
+if z is None:
+    print("Objekt ist das Nichts, also", bool(z))
