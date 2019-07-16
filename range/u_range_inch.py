304fff1b3d03ddee4bcb841f06fdadf7ff36e1a0
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-07-16 12:58:01 +0200

    format tables

--- range/u_range_inch.py
+++ range/u_range_inch.py
@@ -1,2 +1,7 @@
-for i in range(15,45,5):
-    print(i, "Inch", "=", i*2.54, "cm")
+# Umrechnungsfaktor
+inch = 2.54
+
+print(f"{'Inch':>4}{'cm':>12}")
+
+for i in range(15, 41, 5):
+    print(f"{i:>7.1f}{i * inch:>7.1f}")
