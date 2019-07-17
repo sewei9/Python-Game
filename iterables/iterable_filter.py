0246652402c0a0f8971b8fd281e9ac4fe99004df
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-07-17 10:06:24 +0200

    iterable_filter() // list comprehension

---
+++ iterables/iterable_filter.py
@@ -0,0 +1,14 @@
+# FUnktion, die True oder False liefert
+def test(a):
+    if a > 3:
+        return True
+    else:
+        return False
+
+
+# Funktion mehrmals aufruen
+z = filter(test, [5, 6, -2, 0, 12, 3, -5])
+
+# Ausgabe der Werte, die True ergeben
+for element in z:
+    print("True:", element)
