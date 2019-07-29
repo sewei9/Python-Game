7603ee20cb86126c586178bba9fd1cddd4a97ee6
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-07-29 10:33:22 +0200

    Module

---
+++ Eigene Module/modul_neu.py
@@ -0,0 +1,7 @@
+# 1. Eigenenes Modul
+def quadrat(x):
+    erg = x * x
+    return erg
+
+
+    
---
+++ Kommandozeile/kommando_zahl.py
@@ -0,0 +1,10 @@
+import sys
+
+try:
+    x = float(sys.argv[1])
+    y = float(sys.argv[2])
+    z = x + y
+    print("Ergebnis:", z)
+except:
+    print("Parameterfehler")
+    
