6a26ba1f0bfbc91d010bba896a2604d9743a4b31
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-08-12 09:57:15 +0200

    Date & Time

--- Databases/sqlite_like.py
+++ Databases/sqlite_like.py
@@ -7,14 +7,14 @@ cursor = connection.cursor()
 # SQL- Abfragen
 
 # Beliebig viele beliebige Zeichen
-sql = "SELECT * FROM personen WHERE name LIKE 'm%'"
+sql = "SELECT * FROM personen WHERE name LIKE 'm%'"     #Looking for person where the first letter is m
 cursor.execute(sql)
 for dsatz in cursor:
     print(dsatz[0], dsatz[1])
 print()
 
 # Beinhaltet...
-sql = "SELECT * FROM personen WHERE name LIKE '%i%'"
+sql = "SELECT * FROM personen WHERE name LIKE '%i%'"    # Wildcardcharacter %?%
 cursor.execute(sql)
 for dsatz in cursor:
     print(dsatz[0], dsatz[1])
