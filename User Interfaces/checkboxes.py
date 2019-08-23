623d755526faab7ce7941e5b4dbd7a14f8a72642
Sebastian Weiss <sebastian.weiss2@ikea.com>
2019-08-23 13:06:39 +0200

    checkboxes and scalebar

---
+++ User Interfaces/checkboxes.py
@@ -0,0 +1,33 @@
+import tkinter
+
+def ende():
+    main.destroy()
+
+
+def anzeigen():
+    lb["text"] = "Zimmer " + du.get() + " " + mb.get()
+
+
+main = tkinter.Tk()
+
+# Anzeigelabel
+lb = tkinter.Label(main, text = "Zimmer", width=40)
+lb.pack()
+
+# Widget Variables
+du = tkinter.StringVar()
+du.set("ohne Dusche")
+mb = tkinter.StringVar()
+mb.set("ohne Minibar")
+
+# Zwei CHeckbuttons
+cb1 = tkinter.Checkbutton(main, text="Dusche", variable=du, onvalue="mit Dusche", offvalue="ohne Dusche", command=anzeigen)
+cb1.pack()
+
+cb2 = tkinter.Checkbutton(main, text="Minibar", variable=du, onvalue="mit Minibar", offvalue="ohne Minibar", command=anzeigen)
+cb2.pack()
+
+bende = tkinter.Button(main, text="Ende", command=ende)
+bende.pack()
+
+main.mainloop()
